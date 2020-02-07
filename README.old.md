e690bab4288f61a72b23ce23ba756d8a7af39c79
Amra Narmandakh <amra.mandakh@gmail.com>
2020-02-07 16:12:57 -0500

    Project initialize

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# cardShuffleGame
+Dynamo tech Coding Challange - 52 standard card deck with shuffle button and draw cards.
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is Card Shuffle Game</h1>
+    </div>
+  );
+}
+
+export default App;
